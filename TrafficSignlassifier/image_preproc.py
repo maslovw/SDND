dde101d9378a8e5a25359d68280cf421ab8efcfc
Viacheslav Maslov <viacheslav.maslov@esrlabs.com>
2017-06-27 18:20:52 +0200

    generate more train data

--- TrafficSignlassifier/image_preproc.py
+++ TrafficSignlassifier/image_preproc.py
@@ -2,6 +2,45 @@ import bisect
 from skimage import data, img_as_float, img_as_ubyte
 from skimage import exposure
 import numpy as np
+from numba import jit
+
+@jit
+def imadjust_2(src, tol=1, vin=[0,255], vout=(0,255)):
+    # src : input one-layer image (numpy array)
+    # tol : tolerance, from 0 to 100.
+    # vin  : src image bounds
+    # vout : dst image bounds
+    # return : output img
+
+    assert len(src.shape) == 2 ,'Input image should be 2-dims'
+
+    tol = max(0, min(100, tol))
+
+    if tol > 0:
+        # Compute in and out limits
+        # Histogram
+        hist = np.histogram(src,bins=list(range(256)),range=(0,255))[0]
+
+        # Cumulative histogram
+        cum = hist.copy()
+        for i in range(1, 256): cum[i] = cum[i - 1] + hist[i]
+
+        # Compute bounds
+        total = src.shape[0] * src.shape[1]
+        low_bound = total * tol / 100
+        upp_bound = total * (100 - tol) / 100
+        vin[0] = bisect.bisect_left(cum, low_bound)
+        vin[1] = bisect.bisect_left(cum, upp_bound)
+
+    # Stretching
+    scale = (vout[1] - vout[0]) / (vin[1] - vin[0])
+    vs = src-vin[0]
+    vs[src<vin[0]]=0
+    vd = vs*scale+0.5 + vout[0]
+    vd[vd>vout[1]] = vout[1]
+    dst = vd
+
+    return dst
 
 def imadjust_1(src, dst, tol=1, vin=[0,255], vout=(0,255)):
     # src : input one-layer image
@@ -44,7 +83,8 @@ def imadjust(src, tol=1, vin=[0,255], vout=(0,255)):
     dst = np.zeros(src.shape, dtype=np.ubyte)
     #dst = src.copy()
     for i in range(src.shape[2]):
-        imadjust_1(src[:,:,i], dst[:,:,i], tol, vin, vout)
+        #imadjust_2(src[:,:,i], dst[:,:,i], tol, vin, vout)
+        dst[:,:,i] = imadjust_2(src[:,:,i], tol, vin, vout)
     return dst  
     
 def equalizeHistHSV(src):
