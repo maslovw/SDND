a1f27d4e23b759ff7ce347f281edca902e0998e3
Viacheslav Maslov <viacheslav.maslov@esrlabs.com>
2017-08-20 15:22:27 +0200

    trying convolving sliding window

--- Advanced-Lane-Lines/p2.py
+++ Advanced-Lane-Lines/p2.py
@@ -6,6 +6,7 @@ import matplotlib.pylab as pylab
 
 pylab.rcParams['figure.figsize'] = (14, 6)
 
+
 def bgr2rgb(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
@@ -284,7 +285,7 @@ class LinesSearch():
         undist_frame = self.image_binarizer.undistort(self.frame)
         unwarp = self.image_binarizer.unwarp(color_warp)
         ret = cv2.addWeighted(undist_frame, 1, unwarp, 0.3, 0)
-        plot_images((self.frame, ret), im_prep=bgr2rgb)
+        plot_images((bgr2rgb(self.frame), bgr2rgb(ret), self.binary), row=2)
 
     def _binarize(self, frame):
         return self.image_binarizer.binarize(frame)
@@ -301,9 +302,97 @@ class LinesSearch():
             self.right_line.update(self.binary)
 
 
+def find_window_centroids(warped, window_width, window_height, margin):
+    window_centroids = []  # Store the (left,right) window centroid positions per level
+    window = np.ones(window_width)  # Create our window template that we will use for convolutions
+
+    # First find the two starting positions for the left and right lane by using np.sum to get the vertical image slice
+    # and then np.convolve the vertical image slice with the window template
+
+    # Sum quarter bottom of image to get slice, could use a different ratio
+    l_sum = np.sum(warped[int(3 * warped.shape[0] / 4):, :int(warped.shape[1] / 2)], axis=0)
+    l_center = np.argmax(np.convolve(window, l_sum)) - window_width / 2
+    r_sum = np.sum(warped[int(3 * warped.shape[0] / 4):, int(warped.shape[1] / 2):], axis=0)
+    r_center = np.argmax(np.convolve(window, r_sum)) - window_width / 2 + int(warped.shape[1] / 2)
+
+    # Add what we found for the first layer
+    window_centroids.append((l_center, r_center))
+
+    # Go through each layer looking for max pixel locations
+    for level in range(1, (int)(warped.shape[0] / window_height)):
+        # convolve the window into the vertical slice of the image
+        image_layer = np.sum(
+            warped[int(warped.shape[0] - (level + 1) * window_height):int(warped.shape[0] - level * window_height), :],
+            axis=0)
+        conv_signal = np.convolve(window, image_layer)
+        # Find the best left centroid by using past left center as a reference
+        # Use window_width/2 as offset because convolution signal reference is at right side of window, not center of window
+        offset = window_width / 2
+        l_min_index = int(max(l_center + offset - margin, 0))
+        l_max_index = int(min(l_center + offset + margin, warped.shape[1]))
+        l_center = np.argmax(conv_signal[l_min_index:l_max_index]) + l_min_index - offset
+        # Find the best right centroid by using past right center as a reference
+        r_min_index = int(max(r_center + offset - margin, 0))
+        r_max_index = int(min(r_center + offset + margin, warped.shape[1]))
+        r_center = np.argmax(conv_signal[r_min_index:r_max_index]) + r_min_index - offset
+        # Add what we found for that layer
+        window_centroids.append((l_center, r_center))
+
+    return window_centroids
+
+
+def window_mask(width, height, img_ref, center, level):
+    output = np.zeros_like(img_ref)
+    hl = int(img_ref.shape[0] - (level + 1) * height)
+    hh = int(img_ref.shape[0] - level * height)
+    wl = max(0, int(center - width / 2))
+    wh = min(int(center + width / 2), img_ref.shape[1])
+    output[hl:hh, wl:wh] = 1
+    return output
+
+
+def sliding_window(warped, window_width, window_height, margin):
+    window_centroids = find_window_centroids(warped, window_width, window_height, margin)
+
+    # If we found any window centers
+    print(len(window_centroids))
+    if len(window_centroids) > 0:
+
+        # Points used to draw all the left and right windows
+        l_points = np.zeros_like(warped)
+        r_points = np.zeros_like(warped)
+
+        # Go through each level and draw the windows
+        for level in range(0, len(window_centroids)):
+            print(level, window_centroids[level])
+            # Window_mask is a function to draw window areas
+            l_mask = window_mask(window_width, window_height, warped, window_centroids[level][0], level)
+            r_mask = window_mask(window_width, window_height, warped, window_centroids[level][1], level)
+            # Add graphic points from window mask here to total pixels found
+            l_points[(l_points == 255) | ((l_mask == 1))] = 255
+            r_points[(r_points == 255) | ((r_mask == 1))] = 255
+
+        # Draw the results
+        template = np.array(r_points + l_points, np.uint8)  # add both left and right window pixels together
+        zero_channel = np.zeros_like(template)  # create a zero color channel
+        template = np.array(cv2.merge((zero_channel, template, zero_channel)), np.uint8)  # make window pixels green
+        warpage = np.array(cv2.merge((warped, warped, warped)),
+                           np.uint8)  # making the original road pixels 3 color channels
+        output = cv2.addWeighted(warpage, 1, template, 0.5, 0.0)  # overlay the orignal road image with window results
+
+    # If no window centers found, just display orginal road image
+    else:
+        output = np.array(cv2.merge((warped, warped, warped)), np.uint8)
+
+    return output
+
+
 if __name__ == '__main__':
     ls = LinesSearch(ImageSobelHlsBinarizer())
 
     camera_img = cv2.imread('test_images/straight_lines1.jpg')
     ls.search(camera_img)
-    ls.plot()
+    # ls.plot()
+
+    out = sliding_window(ls.binary, 50, 80, 100)
+    plot_images((out, camera_img))
